343a2f24460fda315d115e1994739c836caa4f0a
shambhu Kumar <shambhu.kumar06@GMAIL.COM>
2025-05-24 11:44:40 +0530

    Revert "s"
    This reverts commit 661db427a74b24dad024e96fbb4a49f7cbda5118.

--- root/urls.py
+++ root/urls.py
@@ -2,20 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView
 
-from root.views import api_exception_handler, CrashTestView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/bad/', api_exception_handler, name='api_exception_handler'),
-    path('api/crash/', CrashTestView.as_view()),
 
     path('api/users/', include('accounts.urls')),
-    path('api/test/', include('testdata.urls')),
 ]
-
-
-handler404 = 'root.views.custom_404_view'
-handler500 = 'root.views.custom_500_view'
